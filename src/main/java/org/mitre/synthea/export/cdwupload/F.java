e9ec0cd3a501ba30b69261d9e3b65900975aab28
garypgryan <gpgryan@gmail.com>
2018-07-03 18:06:16 -0400

    added curried function

--- src/main/java/org/mitre/synthea/export/cdwupload/F.java
+++ src/main/java/org/mitre/synthea/export/cdwupload/F.java
@@ -1,4 +1,5 @@
 package org.mitre.synthea.export.cdwupload;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class F {
@@ -16,15 +17,27 @@ public class F {
 	
 	public static Parameter2Function<Integer, String,  String> st = ( Integer len, String inStr) -> Util.singleQuoteStringAndTruncate(inStr,len);
 
-	
-	
 	public static Function<String, String>  m = (String inStr) -> Util.maxDecimal_9_4(inStr);
 	
-	
-	//TODO - getting currying to work so you can pass a maxlen parameter and still have a varargs list of Function
+	//DONE - getting currying to work so you can pass a maxlen parameter and still have a varargs list of Function
 	// that takes one parameter.
-	
 	public static Function<String, String> g = (String inStr) -> Util.truncateTo50AndQuote(inStr);
-	
-}
 
+	public static Function<String, String> nl = (String inStr) -> Util.singleQuoteStringOrNull(inStr);
+
+	// helper function to curry in Java
+	// https://stackoverflow.com/questions/42822820/java-curry-an-existing-static-function
+	public static <T, U, R> Function<T, Function<U, R>> curry(BiFunction<T, U, R> function) {
+		return a -> b -> function.apply(a, b);
+	}
+
+	// curry the function that truncates the input string to the maximum allowed; now you can pass in the maximum length
+	// that allowed and return a function that takes a String and returns a String.   That lets us use the varargs list 
+	// of functions that all have the same signature; i.e. Function<String, String>
+	public static Function<Integer, Function<String, String>> quoteTrunc = curry(Util::singleQuoteStringAndTruncate);
+
+	// write a wrapper around quoteTrunc to make it less verbose
+	public static Function<String, String> qt(int maxLength) {
+		return (quoteTrunc.apply(maxLength));
+	}
+}
