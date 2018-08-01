843ef45a6c2e037899ab7a3339c5c71924e7663d
garypgryan <gpgryan@gmail.com>
2018-08-01 17:19:03 -0400

    bash script to create 10,000 vets across 50 states

---
+++ runusa5.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+mkdir runusa5
+
+./run_synthea -p 182 "Alabama"
+mv output/cdw runusa5/output.Alabama
+./run_synthea -p 33 "Alaska"
+mv output/cdw runusa5/output.Alaska
+./run_synthea -p 251 "Arizona"
+mv output/cdw runusa5/output.Arizona
+./run_synthea -p 110 "Arkansas"
+mv output/cdw runusa5/output.Arkansas
+./run_synthea -p 861 "California"
+mv output/cdw runusa5/output.California
+./run_synthea -p 198 "Colorado"
+mv output/cdw runusa5/output.Colorado
+./run_synthea -p 96 "Connecticut"
+mv output/cdw runusa5/output.Connecticut
+./run_synthea -p 35 "Delaware"
+mv output/cdw runusa5/output.Delaware
+./run_synthea -p 14 "District of Columbia"
+mv output/cdw runusa5/output.DistrictofColumbia
+./run_synthea -p 767 "Florida"
+mv output/cdw runusa5/output.Florida
+./run_synthea -p 337 "Georgia"
+mv output/cdw runusa5/output.Georgia
+./run_synthea -p 55 "Hawaii"
+mv output/cdw runusa5/output.Hawaii
+./run_synthea -p 60 "Idaho"
+mv output/cdw runusa5/output.Idaho
+./run_synthea -p 320 "Illinois"
+mv output/cdw runusa5/output.Illinois
+./run_synthea -p 206 "Indiana"
+mv output/cdw runusa5/output.Indiana
+./run_synthea -p 104 "Iowa"
+mv output/cdw runusa5/output.Iowa
+./run_synthea -p 96 "Kansas"
+mv output/cdw runusa5/output.Kansas
+./run_synthea -p 145 "Kentucky"
+mv output/cdw runusa5/output.Kentucky
+./run_synthea -p 140 "Louisiana"
+mv output/cdw runusa5/output.Louisiana
+./run_synthea -p 58 "Maine"
+mv output/cdw runusa5/output.Maine
+./run_synthea -p 197 "Maryland"
+mv output/cdw runusa5/output.Maryland
+./run_synthea -p 168 "Massachusetts"
+mv output/cdw runusa5/output.Massachusetts
+./run_synthea -p 302 "Michigan"
+mv output/cdw runusa5/output.Michigan
+./run_synthea -p 167 "Minnesota"
+mv output/cdw runusa5/output.Minnesota
+./run_synthea -p 94 "Mississippi"
+mv output/cdw runusa5/output.Mississippi
+./run_synthea -p 221 "Missouri"
+mv output/cdw runusa5/output.Missouri
+./run_synthea -p 45 "Montana"
+mv output/cdw runusa5/output.Montana
+./run_synthea -p 65 "Nebraska"
+mv output/cdw runusa5/output.Nebraska
+./run_synthea -p 108 "Nevada"
+mv output/cdw runusa5/output.Nevada
+./run_synthea -p 53 "New Hampshire"
+mv output/cdw runusa5/output.NewHampshire
+./run_synthea -p 187 "New Jersey"
+mv output/cdw runusa5/output.NewJersey
+./run_synthea -p 79 "New Mexico"
+mv output/cdw runusa5/output.NewMexico
+./run_synthea -p 403 "New York"
+mv output/cdw runusa5/output.NewYork
+./run_synthea -p 352 "North Carolina"
+mv output/cdw runusa5/output.NorthCarolina
+./run_synthea -p 25 "North Dakota"
+mv output/cdw runusa5/output.NorthDakota
+./run_synthea -p 394 "Ohio"
+mv output/cdw runusa5/output.Ohio
+./run_synthea -p 149 "Oklahoma"
+mv output/cdw runusa5/output.Oklahoma
+./run_synthea -p 153 "Oregon"
+mv output/cdw runusa5/output.Oregon
+./run_synthea -p 420 "Pennsylvania"
+mv output/cdw runusa5/output.Pennsylvania
+./run_synthea -p 33 "Rhode Island"
+mv output/cdw runusa5/output.RhodeIsland
+./run_synthea -p 195 "South Carolina"
+mv output/cdw runusa5/output.SouthCarolina
+./run_synthea -p 32 "South Dakota"
+mv output/cdw runusa5/output.SouthDakota
+./run_synthea -p 230 "Tennessee"
+mv output/cdw runusa5/output.Tennessee
+./run_synthea -p 771 "Texas"
+mv output/cdw runusa5/output.Texas
+./run_synthea -p 66 "Utah"
+mv output/cdw runusa5/output.Utah
+./run_synthea -p 22 "Vermont"
+mv output/cdw runusa5/output.Vermont
+./run_synthea -p 353 "Virginia"
+mv output/cdw runusa5/output.Virginia
+./run_synthea -p 277 "Washington"
+mv output/cdw runusa5/output.Washington
+./run_synthea -p 71 "West Virginia"
+mv output/cdw runusa5/output.WestVirginia
+./run_synthea -p 184 "Wisconsin"
+mv output/cdw runusa5/output.Wisconsin
+./run_synthea -p 23 "Wyoming"
+mv output/cdw runusa5/output.Wyoming
+
