1166cb1d573507aba61ca9196bae8d90bafb7c79
garypgryan <gpgryan@gmail.com>
2018-08-01 16:20:07 -0400

    documentation

--- src/main/java/org/mitre/synthea/export/cdwupload/InsertOITLighthouseTables.java
+++ src/main/java/org/mitre/synthea/export/cdwupload/InsertOITLighthouseTables.java
@@ -1,11 +1,11 @@
 package org.mitre.synthea.export.cdwupload;
 
 import java.sql.Connection;
+import java.util.HashMap;
 
 public class InsertOITLighthouseTables {
-	
 
-	public static void load(Connection cn, String dir, Boolean  batchMode ) throws Exception{	
+	public static void load(Connection cn, String dir, Boolean batchMode, HashMap<String,Integer> rowsLoadedPerTable ) throws Exception{	
 		// pass a function to transform each String from the result set in order; F.s will single quote strings, F.d will fix the dates.
 		// F.n just passes the String back
 		// The size of the argument lists must match so each field gets a function. 
@@ -13,13 +13,10 @@ public class InsertOITLighthouseTables {
 	if (batchMode) {
 		cn.setAutoCommit(false);
 	}
-	
-	//TableDelete.delete(cn, "App.Lookup_Patient", " where patientSID > 1 ");
-	TableInsert.load(dir + "lookuppatient.csv", cn, "App.Lookup_Patient",  batchMode,
+	TableInsert.load(dir + "lookuppatient.csv", cn, "App.Lookup_Patient", batchMode,
 			" ( PatientSID, Sta3n, PatientIEN, PatientICN, PatientFullICN,"
-			+ "  PatientName,  TestPatient ) ",
+			+ "  PatientName,  TestPatient ) ", rowsLoadedPerTable,
 			F.s, F.s, F.s, F.s, F.s
 			, F.s, F.s);
 	}
-
 }
