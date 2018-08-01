8e0b6561c44206a552890599665f1e78b28fad85
garypgryan <gpgryan@gmail.com>
2018-08-01 16:03:14 -0400

    added new tables to delete

--- src/main/java/org/mitre/synthea/export/cdwupload/DeleteOITLighthouseTables.java
+++ src/main/java/org/mitre/synthea/export/cdwupload/DeleteOITLighthouseTables.java
@@ -5,9 +5,9 @@ import java.sql.Connection;
 public class DeleteOITLighthouseTables {
 	
 	public static void delete(Connection cn) throws Exception{			
-		TableDelete.delete(cn, "App.Lookup_Patient", " where patientSID > 1 ");
+//		TableDelete.delete(cn, "App.Lookup_Patient", " where patientSID > 1 ");
+		TableDelete.delete(cn, "App.Lookup_Patient", "");
 		cn.commit();
 	}
 
 }
-
