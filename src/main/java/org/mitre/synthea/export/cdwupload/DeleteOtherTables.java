1ddcfecae166c5fc2764da644be5be52ce8fda13
garypgryan <gpgryan@gmail.com>
2018-08-01 16:03:47 -0400

    added new tables to delete

--- src/main/java/org/mitre/synthea/export/cdwupload/DeleteOtherTables.java
+++ src/main/java/org/mitre/synthea/export/cdwupload/DeleteOtherTables.java
@@ -6,8 +6,8 @@ public class DeleteOtherTables {
 	
 	public static void delete(Connection cn) throws Exception{			
 		TableDelete.delete( cn,  "Allergy.AllergicReaction" , "" );
-		TableDelete.delete(cn,"Allergy.Allergy" , "" );
-		TableDelete.delete(  cn,"Allergy.AllergyComment", "" );
+		TableDelete.delete(cn, "Allergy.Allergy" , "" );
+		TableDelete.delete(  cn, "Allergy.AllergyComment", "" );
 		TableDelete.delete(cn, "Appt.Appointment" , "" );
 		TableDelete.delete(cn, "Con.Consult" , "" );
 		TableDelete.delete( cn, "CPRSOrder.CPRSOrder" , "" );
@@ -26,7 +26,19 @@ public class DeleteOtherTables {
 		TableDelete.delete( cn, "SStaff.SStaff" , "" );	
 		
 		TableDelete.delete( cn,"CPRSOrder.OrderedItem", "" );	
+		
+		TableDelete.delete( cn,"Chem.LabChem", "" );
+		TableDelete.delete( cn,"Chem.LabPanel", "" );
+		TableDelete.delete( cn,"Chem.PatientLabChem", "");
+		TableDelete.delete( cn,"Outpat.VProcedure", "" );
+		
+		TableDelete.delete( cn,"Surg.SurgeryPRE", "" );
+		TableDelete.delete(cn, "Surg.SurgeryProcedureDiagnosisCode", "");
+		
+		TableDelete.delete(cn, "RxOut.RxOutpatFill", "");     // this gets filled with a query
+		TableDelete.delete(cn, "Vital.VitalSign", "");
+		
+		
 		cn.commit( );
 	}
 }
-
